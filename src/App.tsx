3e68251d926c5ba1b18e78d3960141caa062f770
Jpedro900 <jpedro.cs09@gmail.com>
2024-12-13 23:15:15 -0300

    feat: App structure complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import React , { useState } from "react";
+import mobileBackground from "./assets/images/bg-main-mobile.png";
+import CardBack from "./components/CardBack/CardBack";
+import CardFront from "./components/CardFront/CardFront";
+import Form from "./components/Form/Form";
+import ThankYou from "./components/ThankYou/ThankYou";
+
 
 function App() {
-  
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
   return (
-    <>
-     <h1 className=' text-2xl text-neutral-900'>Hello World</h1>
-    </>
-  )
+    <div className=" h-screen font-body text-[18px]">
+      <div className=" w-screen relative">
+        <img
+          src={mobileBackground}
+          alt=""
+          className="object-cover w-screen"
+        />
+        <CardBack />
+        <CardFront />
+      </div>
+      {isSubmitted ? <ThankYou /> : <Form setIsSubmitted={setIsSubmitted} />}
+
+    </div>
+  );
 }
 
-export default App
+export default App;
